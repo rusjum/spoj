21acd39142a6dbe43b6b920c67d9f538003b43d9
rusjum <r.jumurov@gmail.com>
2018-06-19 23:13:41 -0700

    check if given graph is the tree. Tree definition + bfs

---
+++ pt07y.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+bool bfs_check(vector<vector<int>> graph, int n) {
+    vector<char> visited(n, 0);
+    deque<int> q = {};
+    visited[0] = 1;
+    q.push_back(0);
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop_front();
+        for (const int & neigh : graph[cur]) {
+            if (visited[neigh] == 0) {
+                visited[neigh] = 1;
+                q.push_back(neigh);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (visited[i] != 1) 
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> graph = {};
+    for (int i = 0; i < n; ++i) {
+        graph.push_back(vector<int>());
+    }
+    int v, u;
+    for (int i = 0; i < m; ++i) {
+        cin >> v >> u;
+        graph[u - 1].push_back(v - 1);
+        graph[v - 1].push_back(u - 1);
+    }
+    bool res = true;
+    if (n == 0 && m == 0) {
+        cout << "YES\n";
+        return 0;
+    }
+    if (m != n - 1) res = false;
+    if (res) {
+        res = bfs_check(graph, n);
+    }
+    
+    if (res) 
+        cout << "YES\n";
+    else 
+        cout << "NO\n";
+    return 0;
+}
+
